bc272b8e760a9696a6d183c1659cbc84fdf321e8
DanielAristy <danielaristy22@gmail.com>
2021-05-19 14:52:23 -0500

    Se instancia la clase Calculadora en el método main, y dentro de la clase Calculadora se crean los getters y setters de los valores

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Calculadora calculadora = new Calculadora();
+        calculadora.setNumero1("10.0");
+        calculadora.setNumero2("2.0");
+        System.out.println(calculadora.getDivision());
     }
 }
