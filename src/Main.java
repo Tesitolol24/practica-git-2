a3517eb5ceffb2f9df5b930438daedcbefbce7f4
Diego Urrego <diegourrg@gmail.com>
2021-05-19 15:16:23 -0500

    Adhición clase Persona e instancia en método Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        Persona persona = new Persona("Diego", "Aristizabal");
+        persona.ObtenerNombre();
     }
 }
